cdf50c446331db4f36af85783c9c86f0f35c1dab
Andrei Shevchuk <andrei@shevchuk.co>
2022-04-11 01:49:20 +0300

    Fix client removal

--- remove-peer.sh
+++ remove-peer.sh
@@ -7,6 +7,9 @@ IFS=$'\n\t'
 
 echo "Current clients:"
 sed -n "s|^\[Peer\] # \(.*\)|\1|p" "${SERVER_CONFIG}"
+
 read -p "Client name to remove: " clientName
 
-sed -i "" -e "s|^\[Peer\] # {$clientName}$|,+4 d" "${SERVER_CONFIG}"
+sed -i -e "|^\[Peer\] # ${clientName}|,+3 d" "${SERVER_CONFIG}"
+
+systemctl restart wg-quick@${WG_INTERFACE}
