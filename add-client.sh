72ea17c97f441f7ec942c301dc3977b7379799ab
Andrei Shevchuk <andrei@shevchuk.co>
2022-04-11 00:09:48 +0300

    Fix variables; make scripts executable

--- add-client.sh
+++ add-client.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 WG_INTERFACE="wg0"
-SERVER_CONFIG="/etc/wireguard/{$WG_INTERFACE}.conf"
+SERVER_CONFIG="/etc/wireguard/${WG_INTERFACE}.conf"
 SERVER_PUB_KEY=`cat /etc/wireguard/public.key`
 
 SCRIPT_DIR=`dirname "$0"`
-CLIENTS_DIR="{$SCRIPT_DIR}/clients"
+CLIENTS_DIR="${SCRIPT_DIR}/clients"
 
-mkdir -p "{$CLIENTS_DIR}"
+mkdir -p "${CLIENTS_DIR}"
 
 read -p "Client name:" clientName
 clientPrivKey=`wg genkey`
@@ -20,15 +20,15 @@ read -p "Enter client IP you want to assign:" clientIP
 cat wg0-client.template \
 | sed -e "s|:CLIENT_IP:|$clientIP|" \
 | sed -e "s|:CLIENT_KEY:|$clientPrivKey|" \
-| sed -e "s|:SERVER_PUB_KEY:|$SERVER_PUB_KEY|" > "{$CLIENTS_DIR}/{$clientName}.conf"
+| sed -e "s|:SERVER_PUB_KEY:|$SERVER_PUB_KEY|" > "${CLIENTS_DIR}/${clientName}.conf"
 
-echo >> "{$SERVER_CONFIG}"
+echo >> "${SERVER_CONFIG}"
 
 cat wg0-peer.template \
 | sed -e "s|:PEER_NAME:|$clientName|" \
 | sed -e "s|:PEER_KEY:|$clientPubKey|" \
-| sed -e "s|:PEER_IP:|$clientIP|" >> "{$SERVER_CONFIG}"
+| sed -e "s|:PEER_IP:|$clientIP|" >> "${SERVER_CONFIG}"
 
-systemctl restart wg-quick@{$WG_INTERFACE}
+systemctl restart wg-quick@${WG_INTERFACE}
 
-qrencode -t ansiutf8 < "{$CLIENTS_DIR}/{$clientName}.conf"
+qrencode -t ansiutf8 < "${CLIENTS_DIR}/${clientName}.conf"
